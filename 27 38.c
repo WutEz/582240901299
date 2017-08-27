2803e03c0a4ae8ad841cfb249e34684e692a5d21
WutEz <31381692+WutEz@users.noreply.github.com>
2017-08-27 16:37:09 +0700

    2-38

---
+++ 27 38.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	printf("%1s\n","58224090129");
+	printf("%1s\n","Arewut Maungvong");
+	printf("%1s\n\n\n\n\n\n\n\n","23/07/39");
+	printf("%120s\n","http://www.npru.ac.th");
+	printf("%57s%s","","BAY BAY");
+	return 0;
+}
